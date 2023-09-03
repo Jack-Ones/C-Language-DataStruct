5787b844de310d8da2a53bb2cfab3db7b5cbe3ac
朔 万 <1459841362@qq.com>
2023-09-03 17:10:20 +0800

    jackone fix

--- 顺序表/Sqlist.c
+++ 顺序表/Sqlist.c
@@ -21,7 +21,7 @@ int LocateElem(Sqlist L, ElemType val) {
 
 //按照位置查找元素，返回元素值
 ElemType GetElem(Sqlist L, int i) {
-	if (i < 0 || i >= L.length) {
+	if (i < 0 || i >=L.length) {
 		printf("int i is wrong->GetElem(L,i)");
 		exit(1);
 	}
@@ -31,13 +31,61 @@ ElemType GetElem(Sqlist L, int i) {
 }
 //在顺序表指定位置插入元素值
 bool SqlistInsert(Sqlist* L, int i, ElemType val) {
-
+	if (i < 0 || i >L->length) {
+		printf("int i is wrong->SqlistInsert(&L,i,val)");
+		exit(1);
+	}
+	else {
+		for (int j = L->length; j > i; j--) {
+			L->data[j] = L->data[j - 1];
+		}
+		L->data[i] = val;
+		L->length++;
+		return true;
+	}
 }
 //删除顺序表指定位置的元素
-ElemType SqlistDelete(Sqlist* L, int i);
+ElemType SqlistDelete(Sqlist* L, int i) {
+	if (i < 0 || i >= L->length) {
+		printf("int i is wrong->SqlistInsert(&L,i,val)");
+		exit(1);
+	}
+	else {
+		for (int j = i; j <L->length-1; j++) {
+			L->data[j] = L->data[j + 1];
+		}
+		L->data[L->length-1] =0;
+		L->length--;
+		return true;
+	}
+}
 //打印顺序表的所有元素
-bool PrintSqlist(Sqlist L);
+bool PrintSqlist(Sqlist L) {
+	if (IsEmpty(L)) {
+		return false;
+	}
+	else
+	{
+		for (int i=0; i < L.length; i++) {
+			printf("%d ", L.data[i]);
+		}
+		printf("\n");
+		return true;
+	}
+}
 //判断顺序表的元素是否为空
-bool IsEmpty(Sqlist L);
+bool IsEmpty(Sqlist L) {
+	if (L.length == 0) {
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 //销毁顺序表
-bool DestroySqlist(Sqlist* L);
+bool DestroySqlist(Sqlist* L) {
+	memset(L->data, 0, L->length);
+	L->length = 0;
+	return true;
+}
---
+++ 顺序表/test.c
@@ -0,0 +1,32 @@
+#include "Sqlist.h"
+#include <stdio.h>
+
+int main(void) {
+	Sqlist L;
+	InitSqlist(&L);
+	SqlistInsert(&L, 0, 1);
+	SqlistInsert(&L, 1, 2);
+	SqlistInsert(&L, 2, 4);
+	SqlistInsert(&L, 3, 8);
+	SqlistInsert(&L, 4, 16);
+	SqlistInsert(&L, 5, 32);
+	int ret=LocateElem(L, 16);
+	printf("%d\n", ret);
+
+	int ret2=GetElem(L, 2);
+	printf("%d\n", ret2);
+
+	SqlistDelete(&L, 2);
+
+	PrintSqlist(L);
+
+	bool ret3 = IsEmpty(L);
+	printf("%d\n", ret3);
+
+	DestroySqlist(&L);
+
+	bool ret4 = IsEmpty(L);
+	printf("%d\n", ret4);
+
+	return 0;
+}
